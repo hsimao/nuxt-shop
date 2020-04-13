1c08aebf3038896fe5b3c02c18272bee19e7b34a
hsimao <e087754958@gmail.com>
2020-04-13 20:26:47 +0800

    update .gitignore

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,4 +1,5 @@
 export default function({ store, req }) {
+  console.log("hello");
   const userLoggedIn = store.getters["auth/loginStatus"];
   if (!userLoggedIn) {
     store.dispatch("auth/setAuthStatus");
